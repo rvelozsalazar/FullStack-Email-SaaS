d166e83509e60344cc023f8a51bf3da1bcd0e9d5
rvelozsalazar <richard.velozsalazar@gmail.com>
2025-09-24 04:53:51 -0700

    Add Aurinko OAuth flow and Account model integration
    - Implemented getAurinkoAuthUrl, exchangeCodeForToken, and getAccountDetails in aurinko.ts
    - Updated route.ts to handle token exchange, fetch account details, and upsert into DB
    - Added Account model in schema.prisma with relation to User

--- src/app/api/aurinko/callback/route.ts
+++ src/app/api/aurinko/callback/route.ts
@@ -1,10 +1,45 @@
 // /api/aurinko/callback 
 
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import { auth } from "@clerk/nextjs/server"
+import { Param } from "@prisma/client/runtime/library"
+import { exchangeCodeForToken, getAccountDetails } from "@/lib/aurinko"
+import { db } from "@/server/db"
 
-export const GET = async (req: Request) => {
+export const GET = async (req: NextRequest) => {
     const {userId} = await auth()
-    console.log('User ID is:', userId)
-    return NextResponse.json({ message: 'Hello mi pequena Sami <3!' })
+    if (!userId) return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
+
+        const params = req.nextUrl.searchParams
+        const status = params.get('status')
+        if (status != 'success') return NextResponse.json({ message: 'Failed to link account' }, { status: 400 })
+
+        // get code to exchange for access token
+        const code = params.get('code')
+        if (!code) return NextResponse.json({ message: 'No code provided' }, { status: 400 })
+        const token = await exchangeCodeForToken(code)
+        if (!token) return NextResponse.json({ message: 'Failed to exchange code for token' }, { status: 500 })
+
+        const accountDetails = await getAccountDetails(token.accessToken)
+
+
+        await db.account.upsert({
+            where: {
+                id: token.accountId.toString()
+            },
+            update: {
+                accessToken: token.accessToken
+            },
+            create: {
+                id: token.accountId.toString(),
+                userId,
+                emailAddress: accountDetails.email,
+                name: accountDetails.name,
+                accessToken: token.accessToken
+            }
+        })
+
+
+
+    return NextResponse.redirect(new URL('/mail', req.url))
 }
